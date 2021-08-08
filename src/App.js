5f4b6b752ae4d3539cd58a390d5121ff62d8f1ca
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-08 17:44:07 +0200

    Setting up the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+
+import ContactList from "./components/ContactList";
+import ContactInput from "./components/ContactInput";
+
 function App() {
-    return <div className="App">App</div>;
+    const [ contacts, setContacts ] = useState(null);
+    const [ loading, setLoading ] = useState(false);
+
+    const fetchContacts = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch("http://localhost:5000/contacts");
+            const data = await res.json();
+            setContacts(data);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => fetchContacts(), []);
+
+    return (
+        <div className="container">
+            <h1 className="page-title">Contacts</h1>
+            <ContactInput contacts={contacts} setContacts={setContacts} />
+            {/* <input value={site} onChange={(e) => setSite(e.target.value)} />
+      <button onClick={findSite}>Find</button> */}
+            <ContactList
+                contacts={contacts}
+                setContacts={setContacts}
+                loading={loading}
+            />
+        </div>
+    );
 }
 
 export default App;
